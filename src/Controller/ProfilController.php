07528f69967566d2fa9772468838ac11a87d0e0d
badisbms <79091956+badisbms@users.noreply.github.com>
2021-11-26 10:23:11 +0100

    ajout edition profil

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/profil")
+ */
+class ProfilController extends AbstractController
+{
+    /**
+     * @Route("/", name="profil")
+     */
+    public function profil() : Response {
+        $user = $this->getUser();
+        return $this->render('profil/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+
+      /**
+     * @Route("/{id}/edit", name="profil_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('profil_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profil/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+
+     /**
+     * @Route("/{id}", name="profil_delete", methods={"POST"})
+     */
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('profil_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    // /**
+    //  * @Route("/new", name="profil_new", methods={"GET", "POST"})
+    //  */
+    // public function new(Request $request, EntityManagerInterface $entityManager): Response
+    // {
+    //     $user = new User();
+    //     $form = $this->createForm(UserType::class, $user);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->persist($user);
+    //         $entityManager->flush();
+
+    //         return $this->redirectToRoute('profil_index', [], Response::HTTP_SEE_OTHER);
+    //     }
+
+    //     return $this->renderForm('profil/new.html.twig', [
+    //         'user' => $user,
+    //         'form' => $form,
+    //     ]);
+    // }
+
+    // /**
+    //  * @Route("/{id}", name="profil_show", methods={"GET"})
+    //  */
+    // public function show( User $user): Response
+    // {
+    //     $user = $this->getUser();
+    //     return $this->render('tools/sidebar.html.twig', [
+    //         'user' => $user,
+    //     ]);
+    // }
+
+  
+
+   
+}
