32e6075c4df1427b9e134d8895abedca78d73f6d
badisbms <79091956+badisbms@users.noreply.github.com>
2021-12-03 10:05:58 +0100

    knp paginator

--- src/Controller/EncyclopedieController.php
+++ src/Controller/EncyclopedieController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PlantRepository;
 use App\Repository\ImagesRepository;
 use App\Entity\Plant;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @Route("/encyclopedie")
@@ -18,10 +20,19 @@ class EncyclopedieController extends AbstractController
     /**
      * @Route("/", name="encyclopedie", name="encyclopedie_index", methods={"GET"})
      */
-    public function index(PlantRepository $plantRepository): Response
+    public function index(Request $request, PlantRepository $plantRepository, PaginatorInterface $paginator): Response
     {
+
+        $donnees = $this->getDoctrine()->getRepository(Plant::class)->findBy([],['id' => 'ASC']);
+
+        $plants = $paginator->paginate(
+            $donnees, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            15 // Nombre de résultats par page
+        );
+
         return $this->render('encyclopedie/index.html.twig', [
-            'plants' => $plantRepository->findAll(),
+            'plants' => $plants,
         ]);
     }
 
